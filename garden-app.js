1982aac44460acf25ad10e7cc0daf45c61f9d580
kentenroth <kentencroth@yahoo.com>
2018-05-27 14:45:18 -0500

    displaying
    Displaying the hardcoded plants array, days until harvest, and remove button to the screen.

--- garden-app.js
+++ garden-app.js
@@ -1,1 +1,34 @@
-const plants = []
+const plants = [{
+    name: 'Corn',
+    harvest: 100
+},{
+    name: 'Radish',
+    harvest: 35,
+}, {
+    name: 'Onion',
+    harvest: 85
+}]
+
+const createPlantsDOM = function (plant) {
+    const plantList = document.createElement('div')
+    const plantName = document.createElement('span')
+    const plantHarvest = document.createElement('span')
+    const harvestedPlant = document.createElement('button')
+
+    plantName.textContent = plant.name 
+    plantList.appendChild(plantName)
+
+    plantHarvest.textContent = plant.harvest
+    plantList.appendChild(plantHarvest)
+
+    harvestedPlant.textContent = 'x'
+    plantList.appendChild(harvestedPlant)
+
+    return plantList
+}
+
+plants.forEach(function (plant) {
+    const plantList = createPlantsDOM(plant)
+    document.querySelector('#planted-list').appendChild(plantList)
+
+})
